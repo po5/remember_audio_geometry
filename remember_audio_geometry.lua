c565facd0aa08d5fa0f4465f436916b0ff10265e
Eva <evauwu@riseup.net>
2020-08-03 00:29:11 +0200

    Reset geometry between directories

--- remember_audio_geometry.lua
+++ remember_audio_geometry.lua
@@ -1,3 +1,6 @@
+local utils = require 'mp.utils'
+local last_dir = ""
+
 mp.observe_property("current-window-scale", "number", function(_, scale)
     if scale == nil then return end
     if not is_audio_file() then return end
@@ -5,6 +8,14 @@ mp.observe_property("current-window-scale", "number", function(_, scale)
     width, height = mp.get_osd_size()
     if (width or 0) < 1 then return end
     mp.set_property("geometry", width .. "x" .. height)
+end)
+
+mp.add_hook("on_load", 10, function ()
+    local dir, filename = utils.split_path(mp.get_property("stream-open-filename", ""))
+    if dir ~= last_dir and last_dir ~= "" then
+        mp.set_property("geometry", "50%:50%")
+    end
+    last_dir = dir
 end)
 
 -- https://github.com/CogentRedTester/mpv-coverart/blob/master/coverart.lua
